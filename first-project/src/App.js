39df5b9f5b2d58c9330a6751aa9bf8bfd2893747
Tawsiful123 <tawsifulislam13502@gmail.com>
2024-12-10 01:35:59 +0600

    Hii

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -6,6 +6,9 @@ import {Shop} from './Pages/Shop';
 import {Product} from './Pages/Product';
 import {LoginSignup} from './Pages/LoginSignup';
 import {ShopCategory} from './Pages/ShopCategory';
+import {men_banner} from './Components/Assests/banner_mens.png';
+import {women_banner} from './Components/Assests/banner_women.png';
+import {kid_banner} from './Components/Assests/banner_mens.png';
 
 
 function App() {
@@ -14,9 +17,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Shop />} />
-        <Route path="/mens" element={<ShopCategory category="men"/>} />
-        <Route path="/womens" element={<ShopCategory category="women"/>} />
-        <Route path="/kids" element={<ShopCategory category="kids"/>} />
+        <Route path="/mens" element={<ShopCategory banner={men_banner} category="men"/>} />
+        <Route path="/womens" element={<ShopCategory banner={women_banner} category="women"/>} />
+        <Route path="/kids" element={<ShopCategory bannner={kid_banner} category="kids"/>} />
         <Route path="/cart" element={<Product />} />
         
         <Route path="/cart" element={<Cart />} />
